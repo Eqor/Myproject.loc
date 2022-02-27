0e24b88f17f73677323800245a076cad39d389cf
KEE <bishop1996@mail.ru>
2022-02-27 22:33:53 +0300

    Added Comments and comment editor

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -1,6 +1,14 @@
-<?php include __DIR__ . '/../header.php'; ?>
+
+<?php
+/**
+ * @var \MyProject\Models\Articles\Article $article
+ */
+include __DIR__ . '/../header.php'; ?>
     <h1><?= $article->getName() ?></h1>
     <p><?= $article->getText() ?></p>
     <p>Автор: <?= $article->getAuthor()->getNickname() ?></p>
-<?php include __DIR__ . '/../footer.php'; ?>
+<?php if (!empty($user) && $user->isAdmin()): ?>
+      <a href="http://myproject.loc/articles/<?= $article->getId() ?>/edit">Изменить
+          <?php endif; ?>
 
+<?php include __DIR__ . '/../footer.php'; ?>
