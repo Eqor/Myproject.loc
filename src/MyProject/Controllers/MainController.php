7cf31b6b5c731f45178ce0ceeb0322ca014f8a55
KEE <bishop1996@mail.ru>
2022-03-21 17:43:16 +0300

    Added pagination

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -18,13 +18,14 @@ class MainController extends AbstractController
         $pagesCount = Article::getPagesCount(5);
         $this->view->renderHtml('main/main.php', [
             'articles' => Article::getPage($pageNum, 5),
+            'user' => UsersAuthService::getUserByToken(),
             'previousPageLink' => $pageNum > 1
                 ? '/' . ($pageNum - 1)
                 : null,
             'nextPageLink' => $pageNum < $pagesCount
                 ? '/' . ($pageNum + 1)
-                : null,
-            'user'=> UsersAuthService::getUserByToken()
+                : null
+
         ]);
     }
 
